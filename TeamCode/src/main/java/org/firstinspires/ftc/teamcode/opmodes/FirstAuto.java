4bf4f1fe41b05133b6d609d0b540aa5c67acf84a
Avaninder Bhaghayath <charizardavi@gmail.com>
2022-12-26 19:18:00 -0500

    Cleaned up Turntable

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/FirstAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/FirstAuto.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
@@ -8,9 +9,11 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.teamcode.roadrunner.drive.MecanumDrive;
 
 @Autonomous(name = "FirstAuto")
+@Config
 public class FirstAuto extends LinearOpMode {
     public static double DISTANCE = 50;
 
+    @Override
     public void runOpMode() throws InterruptedException {
         MecanumDrive drive = new MecanumDrive(hardwareMap);
 
@@ -19,6 +22,8 @@ public class FirstAuto extends LinearOpMode {
 
         Trajectory trajectoryBackward =
                 drive.trajectoryBuilder(trajectoryForward.end()).back(DISTANCE).build();
+
+        waitForStart();
 
         while (opModeIsActive() && !isStopRequested()) {
             drive.followTrajectory(trajectoryForward);
