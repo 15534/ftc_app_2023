a7388e09232a6347bb0c253c5e13a27657f42a8c
Avaninder Bhaghayath <charizardavi@gmail.com>
2023-01-10 18:30:22 -0500

    Almost pid

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/LiftLimits.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/LiftLimits.java
@@ -1,38 +1,37 @@
-//package org.firstinspires.ftc.teamcode.test;
-//
-//import com.acmerobotics.dashboard.config.Config;
-//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-//import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-//import com.qualcomm.robotcore.hardware.DcMotor;
-//import com.qualcomm.robotcore.hardware.DcMotorEx;
-//
-//import org.firstinspires.ftc.teamcode.subsystems.LiftPID;
-//
-//@TeleOp(name = "LiftLimits")
-//@Config()
-//public class LiftLimits extends LinearOpMode {
-//
-//    public static double leftPowerInitial = 0.1; // counterclockwise
-//    public static double rightPowerInitial = -0.1; // clockwise
-//
-//    public static double leftPos = 20; // counterclockwise
-//    public static double rightPos = -20; // clockwise
-//
-//    private LiftPID lift = new LiftPID();
-//
-//    @Override
-//    public void runOpMode() throws InterruptedException {
-//        waitForStart();
-//
-//        lift.init(hardwareMap);
-//
-//        while (opModeIsActive() && !isStopRequested()) {
-//
-//
-//            telemetry.addData("Left: ", left.getCurrentPosition());
-//            telemetry.addData("Right: ", right.getCurrentPosition());
-//
-//            telemetry.update();
-//        }
-//    }
-//}
+package org.firstinspires.ftc.teamcode.test;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import org.firstinspires.ftc.teamcode.subsystems.LiftPID;
+
+@TeleOp(name = "LiftLimits")
+@Config()
+public class LiftLimits extends LinearOpMode {
+    public static int liftTarget = 20;
+
+    private LiftPID lift = new LiftPID();
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+        waitForStart();
+
+        lift.init(hardwareMap);
+        lift.setLiftPosition(liftTarget);
+        while (opModeIsActive() && !isStopRequested()) {
+            lift.setLiftPosition(liftTarget);
+
+            lift.updateLiftPosition();
+
+            telemetry.addData("current ", lift.getPosition());
+            telemetry.addData("target ", liftTarget);
+            telemetry.addData("right ", lift.getRight());
+            telemetry.addData("left ", lift.getLeft());
+
+            telemetry.update();
+        }
+    }
+}
