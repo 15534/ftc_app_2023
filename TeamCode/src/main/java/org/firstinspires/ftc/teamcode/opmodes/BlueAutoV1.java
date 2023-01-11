db3a1c30ca401923bf9de76a3be1411ba855ad3b
byran.tech <61983584+Hello9999901@users.noreply.github.com>
2023-01-11 10:26:31 -0500

    optimized values for lift, belt and blueauto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/BlueAutoV1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/BlueAutoV1.java
@@ -63,10 +63,11 @@ public class BlueAutoV1 extends LinearOpMode {
         Park
         */
 
-        firstHighPole = drive.trajectoryBuilder(startingPos).lineTo(new Vector2d(12, 24)).build();
+        firstHighPole = drive.trajectoryBuilder(startingPos).lineTo(new Vector2d(11, 24)).build();
 
         firstConeStack =
                 drive.trajectoryBuilder(firstHighPole.end())
+                        .splineTo(new Vector2d(24, 12), Math.toRadians(0))
                         .splineTo(new Vector2d(52, 12), Math.toRadians(0))
                         .build();
 
