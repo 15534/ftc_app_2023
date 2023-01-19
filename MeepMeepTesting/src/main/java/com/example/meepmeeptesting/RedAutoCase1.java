339bc7e91544ebd3a550e5e00f5f312d23ccbfc6
Hello9999901 <byran8855@gmail.com>
2023-01-19 18:21:28 -0500

    Update RedAutoCase1.java

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/RedAutoCase1.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/RedAutoCase1.java
@@ -10,20 +10,29 @@ import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class RedAutoCase1 {
     public static void main(String[] args) {
-        MeepMeep meepMeep = new MeepMeep(800);
+        MeepMeep meepMeep = new MeepMeep(600);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .setDimensions(16, 16)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(12, -62, Math.toRadians(90)))
-                                .lineTo(new Vector2d(12, -24))
+                        drive.trajectorySequenceBuilder(new Pose2d(36, -62, 0))
+                                .lineTo(new Vector2d(36, 0))
                                 .waitSeconds(0.5)
-                                .lineTo(new Vector2d(12, -12))
-                                .turn(Math.toRadians(-90))
+                                .splineToConstantHeading(new Vector2d(42,-12), Math.toRadians(0))
+                                .lineTo(new Vector2d(60, -12))
+                                .lineTo(new Vector2d(24, -12))
                                 .lineTo(new Vector2d(60, -12))
                                 .build()
+//                        drive.trajectorySequenceBuilder(new Pose2d(36, -62, Math.toRadians(90)))
+//                                .lineTo(new Vector2d(36, 0))
+//                                .waitSeconds(0.5)
+//                                .splineToLinearHeading(new Pose2d(42,-12), Math.toRadians(0))
+//                                .lineTo(new Vector2d(60, -12))
+//                                .lineTo(new Vector2d(24, -12))
+//                                .lineTo(new Vector2d(60, -12))
+//                                .build()
                 );
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_KAI_DARK)
