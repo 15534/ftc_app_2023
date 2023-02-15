bbba1c02a5457fa18ef5f8944f00f1794827637b
Hello9999901 <byran8855@gmail.com>
2023-02-14 22:33:40 -0500

    <FEAT>(MEEPMEEP/AUTO): added two new pathing options

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/SplineAutoNoHitWall.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/SplineAutoNoHitWall.java
@@ -6,7 +6,7 @@ import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
-public class SplineAuto45Deg {
+public class SplineAutoNoHitWall {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
@@ -17,14 +17,8 @@ public class SplineAuto45Deg {
                         .followTrajectorySequence(
                                 drive ->
                                         drive.trajectorySequenceBuilder(new Pose2d(42, -62, Math.toRadians(90.00)))
-                                                .splineTo(new Vector2d(38.30, -54.96), Math.toRadians(107.57))
-                                                .splineTo(new Vector2d(35.43, -34.28), Math.toRadians(98.27))
-                                                .splineTo(new Vector2d(34.85, -18.77), Math.toRadians(90.80))
-                                                .splineTo(new Vector2d(28.34, -6.32), Math.toRadians(117.61))
-                                                .splineToLinearHeading(new Pose2d(34.47, -13.02, Math.toRadians(180)), Math.toRadians(-9.46))
-                                                .lineTo(new Vector2d(48.06, -12.26))
-                                                .lineTo(new Vector2d(60, -12.26))
-                                                .lineTo(new Vector2d(-60, -12.26))
+                                                .lineTo(new Vector2d(33.70, -52.09))
+                                                .lineTo(new Vector2d(34.66, -0.77))
                                         .build());
 
 
